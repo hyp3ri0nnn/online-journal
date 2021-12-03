4d25c3402f00b0492d93c4e4d7bf4ee78da3567c
Mahmut <mahmutseliz26@gmail.com>
2021-12-04 01:49:16 +0300

    profile section will be continue

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Register your models here.
 from .models import Entry, Topic
+
 
 admin.site.register(Topic)
 admin.site.register(Entry)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "image"]
+    class Meta:
+        model = Profile
+
+
+admin.site.register(Profile, ProfileAdmin)
